2480fac6a5e813aa9e0427262314bc621973f4a1
jwoff78 <jwelshoffical78@gmail.com>
2020-06-09 01:46:28 -0500

    Code Fix so github can compile

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OrkCore.Interface;
-using System.IO;
-using JJGE_Core.Interface;
 using OpenGL;
 using Vec3 = OpenGL.Vector3;
 
