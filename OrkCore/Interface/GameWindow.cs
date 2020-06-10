aa435449155a2fd2cbecdb8ca1e6264a97b6a83d
NIL-Entertainment <61469501+NIL-Entertainment@users.noreply.github.com>
2020-06-11 01:38:49 +0200

    SDL WIndow Rendering + Event Handler

--- OrkCore/Interface/GameWindow.cs
+++ OrkCore/Interface/GameWindow.cs
@@ -15,20 +15,74 @@ namespace OrkEngine.Interface
         string Title;
         int winWidth = 1600, winHeight = 900;
         bool closed = false;
+        Func<object> Start;
+        Func<object> Update;
 
-        public GameWindow(string title = "OrkRender", int w = 1600, int h = 900)
+        public GameWindow(Func<object> start, Func<object> update, string title = "OrkRender", int w = 1280, int h = 720)
         {
             winWidth = w;
             winHeight = h;
             Title = title;
+            Start = start;
+            Update = update;
 
-            Console.WriteLine("Hello, Mother Fucker");
-
-            SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
+            {
+                Console.WriteLine("SDL ded ono [ERROR: '{0}']", SDL.SDL_GetError());
+                return;
+            }
 
             var window = SDL.SDL_CreateWindow(Title, SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, winWidth, winHeight, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
-            SDL.SDL_Delay(5000);
+            var renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+            var Image = SDL_image.IMG_LoadTexture(renderer, "ork_engine.png");
+
+
+
+            #region ork
+            SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL.SDL_RenderClear(renderer);
+
+            SDL.SDL_Rect r1;
+            r1.x = 0;
+            r1.y = 0;
+            r1.w = 200;
+            r1.h = 200;
+
+            SDL.SDL_Rect r2;
+            r2.x = 450;
+            r2.y = 150;
+            r2.w = 400;
+            r2.h = 400;
+
+            SDL.SDL_RenderCopy(renderer, Image, ref r1, ref r2);
+
+            SDL.SDL_RenderPresent(renderer);
+            #endregion
+
+            SDL.SDL_Event e;
+            bool quit = false;
+
+            Start();
+
+            /* MAIN LOOP */
+
+            while (!quit)
+            {
+                while (SDL.SDL_PollEvent(out e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+                    }
+                }
+                Update();
+            }
+            SDL.SDL_DestroyTexture(Image);
+            SDL.SDL_DestroyRenderer(renderer);
             SDL.SDL_DestroyWindow(window);
 
             SDL.SDL_Quit();
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 using OrkEngine.Interface;
 using System.IO;
 using OpenGL;
-using Vec3 = OpenGL.Vector3;
 
 namespace Tests
 {
     class Program
     {
-        GameWindow gameWindow = new GameWindow();
         Renderable vcube;
         Renderable r;
 
@@ -22,54 +20,20 @@ namespace Tests
         public static void Main(string[] args)
         {
             //Console.WriteLine("Hello, world");
-            //new Program();
+            new Program();
         }
         public Program()
         {
-            
+            GameWindow g = new GameWindow(Start, Update);
         }
         public object Start()
         {
             Console.WriteLine("Start");
-            /*vcube = Renderable.Cube;
-            vcube.texture = new Texture("vroooom.png");
-            vcube.PrepareVBOs();
-            vcube.rotation = new Vec3(0.5f, 0.5f, 0);
-
-            gameWindow.AddRenderable(vcube);*/
-
-            r = Renderable.LoadFromFile(@"vroooomcube2.obj");
-            r.PrepareVBOs();
-            r.texture = new Texture(@"Cube9_auv.png");
-
-            Console.WriteLine("ELEMENTS: " + r.elements.Length);
-            Console.WriteLine("UVS: " + r.uvs.Length);
-
-            //gameWindow.AddRenderable(r);
 
             return null;
         }
         public object Update()
         {
-            counter[0]++;
-
-            if (counter[0] > counter[1]) {
-                counter[0] = 0;
-
-                r.rotation.x += 0.05f;
-                r.rotation.y += 0.025f;
-
-                /*vcube.rotation.x += 0.05f;
-                vcube.rotation.y += 0.025f;
-
-                if (mode)
-                    vcube.position.x += (limits[1] - vcube.position.x) / 3;
-                else
-                    vcube.position.x += (limits[0] - vcube.position.x) / 3;
-
-                if (vcube.position.x > 1.45f || vcube.position.x < -1.45f)
-                    mode = !mode;*/
-            }
             return null;
         }
     }
