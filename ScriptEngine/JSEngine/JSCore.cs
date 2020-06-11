1dcdadb3990106527ae83ec2624bb948598e1eb0
jwoff78 <jwelshoffical78@gmail.com>
2020-06-11 12:22:14 -0500

    JSCore Engine

--- ScriptEngine/JSEngine/JSCore.cs
+++ ScriptEngine/JSEngine/JSCore.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Jint;
+using Jint.Parser;
 namespace ScriptEngine.JSEngine
 {
-    class JSCore
+    public class JSCore
     {
+        public Engine jsEngine = new Engine();
+
+        public void DoString(string script)
+        {
+            jsEngine.Execute(script);
+        }
+
+        public void DoString(string script, ParserOptions options)
+        {
+            jsEngine.Execute(script, options);
+        }
+
+        public void DoFile(string fileName)
+        {
+
+        }
+
+        public void DoFiles(string[] files)
+        {
+
+        }
     }
 }
